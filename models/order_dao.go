58d9371a7cb4a848e96152f8590c08687fdbd90a
Scott Li <scottliyq@gmail.com>
2016-06-07 14:35:56 +0800

    add merchantStatus return

--- models/order_dao.go
+++ models/order_dao.go
@@ -9,7 +9,7 @@ import (
 //Gets all orders by order status and merchant status
 func GetOrdersByStatus(orderStatus int, merchantStatus int) (orders []vo.OrderVo, err error) {
 	o := orm.NewOrm()
-	_, err = o.Raw("SELECT order.id, order.order_number,`person`.name,matrix.merchant_user_name,`person`.mobile_number,`order`.principal_balance,merchant.merchant_name, order.lov_order_status,order.order_time FROM `fin_order` as `order` inner join `fin_person` as`person` on `order`.person_id=`person`.id inner join fin_merchant as merchant on merchant.id=order.merchant_id inner join fin_merchant_user_matrix as matrix on matrix.person_id=order.person_id and matrix.merchant_id=order.merchant_id WHERE `order`.lov_order_status =? and order.lov_merchant_status=? order by`order`.order_time desc", orderStatus, merchantStatus).QueryRows(&orders)
+	_, err = o.Raw("SELECT order.id, order.order_number,`person`.name,matrix.merchant_user_name,`person`.mobile_number,`order`.principal_balance,merchant.merchant_name, order.lov_order_status,order.order_time,order.lov_merchant_status FROM `fin_order` as `order` inner join `fin_person` as`person` on `order`.person_id=`person`.id inner join fin_merchant as merchant on merchant.id=order.merchant_id inner join fin_merchant_user_matrix as matrix on matrix.person_id=order.person_id and matrix.merchant_id=order.merchant_id WHERE `order`.lov_order_status =? and order.lov_merchant_status=? order by`order`.order_time desc", orderStatus, merchantStatus).QueryRows(&orders)
 
 	return
 }
