c4268692f2a2682178b990ec4f6c683f1e88bdf4
Scott Li <scottliyq@gmail.com>
2016-07-11 11:58:32 +0800

    add merchant_user_name

--- models/payment_dao.go
+++ models/payment_dao.go
@@ -103,14 +103,14 @@ func CalcDuration(strPaymentDate string, strCurrentDate string) (duration int, e
 func GetPaymentsByStatus(merchantId int, paymentStatus int, delayStatus int) (payments []vo.PaymentVo, err error) {
 	o := orm.NewOrm()
 
-    qb, _ := orm.NewQueryBuilder("mysql")
-	qb.Select("payment.id, finorder.product_name,person.name,person.mobile_number, payment.lov_payment_status,payment.payment_date,payment.payment_amount,payment.payment_stage, payment.payment_number, payment.delay_payment_fine, payment.lov_delay_status")
+	qb, _ := orm.NewQueryBuilder("mysql")
+	qb.Select("payment.id,matrix.merchant_user_name, finorder.product_name,person.name,person.mobile_number, payment.lov_payment_status,payment.payment_date,payment.payment_amount,payment.payment_stage, payment.payment_number, payment.delay_payment_fine, payment.lov_delay_status")
 	qb.From("fin_payment as payment").InnerJoin("fin_person as person").On("payment.person_id=person.id")
 	qb.InnerJoin("fin_order as finorder").On("payment.order_id=finorder.id")
 	qb.InnerJoin("fin_merchant_user_matrix as matrix").On("person.id=matrix.person_id").And("matrix.merchant_id=?")
-	
+
 	var r orm.RawSeter
-	
+
 	if paymentStatus >= 0 && delayStatus >= 0 {
 		qb.Where("payment.lov_payment_status=?").And("payment.lov_delay_status=?")
 		qb.OrderBy("payment.payment_date").Asc()
